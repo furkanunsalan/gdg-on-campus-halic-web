a1cc32dc70e05229b571cb07c9f5ed3c910259cf
Furkan Ünsalan <me@furkanunsalan.dev>
2024-09-22 01:27:25 +0300

    second initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import {CustomNavbar} from "@/components/custom-navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <p>Hello</p>
+    <>
+      <p>Hello</p>
+      <CustomNavbar />
+    </>
   );
 }
